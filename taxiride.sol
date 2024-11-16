6011f5c3e4ded575d3cc6b694a83be8eeb534b25
2022179 <2022179@student.cct.ie>
2024-11-16 18:15:24 +0000

    structure to present the ride

--- taxiride.sol
+++ taxiride.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity version;
+pragma solidity  ^0.8.0;
 //Define the taxiride contract
 contract TaxiRide{
-    address public owner,// the platform owner
-    uintpublic platformBalance; // commisions
+    address public owner;// the platform owner
+    uint public platformBalance; // commisions
+    // Struct to represent a ride
+    struct Ride {
+address payable rider;// riders address here
+address payable driver;// Driver address here
+uint fare;// ride fare here
+bool isAccepted;// option to see if driver has accept the ride
+bool isCompleted;//option to see if ride has been accepted by rider
+}
 }
