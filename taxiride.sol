dedeb83f9214ffae3dfa4c7ded526f49cfa910bd
2022179 <2022179@student.cct.ie>
2024-11-16 18:01:16 +0000

    created  a contract for taxi ride

--- taxiride.sol
+++ taxiride.sol
@@ -1,1 +1,7 @@
 // SPDX-License-Identifier: MIT
+pragma solidity version;
+//Define the taxiride contract
+contract TaxiRide{
+    address public owner,// the platform owner
+    uintpublic platformBalance; // commisions
+}
